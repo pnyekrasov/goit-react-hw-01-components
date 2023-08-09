9b8c63c15bb9af1aa558de554b86c624bf627326
pnyekrasov <pnekrasov1910@gmail.com>
2023-08-09 22:54:19 +0300

    final 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendsList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import user from './Data/user.json';
-import data from './Data/data.json';
-import friends from './Data/friends.json';
-import transactions from './Data/transactions.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
